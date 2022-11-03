01e4abf7a9a4e6c97dd41141e18d7db79ae025c4
Tim Dang <tim.dang@techvify.com.vn>
2022-11-03 10:30:39 +0700

    huhu

--- api/api.ts
+++ api/api.ts
@@ -6,7 +6,7 @@ let urls = {
   production: 'https://muabanaccgenshin.online/'
 };
 const api = Axios.create({
-  baseURL: urls[process.env.NODE_ENV],
+  baseURL: urls[process.env.APP_ENV],
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
--- api/formData/apiFormData.ts
+++ api/formData/apiFormData.ts
@@ -6,7 +6,7 @@ let urls = {
   production: 'https://muabanaccgenshin.online/'
 };
 const apiFormData = Axios.create({
-  baseURL: urls[process.env.NODE_ENV],
+  baseURL: urls[process.env.APP_ENV],
   headers: {
     Accept: '*/*',
     'Content-Type': 'multipart/form-data'
