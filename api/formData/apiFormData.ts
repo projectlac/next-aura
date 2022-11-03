1fcb572dd4bc3b9b2bd6bc945a7f14d86b63e96c
Tim Dang <tim.dang@techvify.com.vn>
2022-11-03 11:04:29 +0700

    fix env

--- api/formData/apiFormData.ts
+++ api/formData/apiFormData.ts
@@ -1,10 +1,5 @@
 import Axios from 'axios';
 
-let urls = {
-  test: `http://localhost:8888`,
-  development: 'https://muabanaccgenshin.online/',
-  production: 'https://muabanaccgenshin.online/'
-};
 const apiFormData = Axios.create({
   baseURL: 'https://muabanaccgenshin.online/',
   headers: {
