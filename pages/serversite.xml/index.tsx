ba1266a790dc35b319cc8da6585b85b4b6a84cbc
Tim Dang <tim.dang@techvify.com.vn>
2022-11-07 14:07:10 +0700

    add seo

---
+++ pages/serversite.xml/index.tsx
@@ -0,0 +1,56 @@
+import { queryAllAccountForSiteMap } from 'api/apiAccount/account';
+
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
+
+export const GetPost = async () => {
+  const data = await queryAllAccountForSiteMap();
+
+  return data.data.data;
+};
+export const getServerSideProps: any = async (ctx) => {
+  const siteUrl = 'https://GenshinViet.com';
+  const news: any = await GetPost();
+  const fieldHome: ISitemapField[] = [
+    {
+      loc: `${siteUrl}/`,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: `${siteUrl}/404`,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: `${siteUrl}/topup`,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: `${siteUrl}/topup-genshin`,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: `${siteUrl}/account/vip`,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: `${siteUrl}/account/reroll`,
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: `${siteUrl}/account/random`,
+      lastmod: new Date().toISOString()
+    }
+  ];
+
+  const fieldsNews: ISitemapField[] = news?.map((data: any) => ({
+    loc: `${siteUrl}/account/details/${data.slug}`,
+    lastmod: new Date().toISOString()
+  }));
+
+  const fields = fieldsNews.concat(fieldHome);
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+export default function Site() {
+  //console
+}
