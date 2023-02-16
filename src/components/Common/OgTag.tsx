743070a7d7287c81108b3dadbbdc85ba7fb87685
projectlac <dft1711198@gmail.com>
2023-02-16 21:05:17 +0700

    huhu

--- src/components/Common/OgTag.tsx
+++ src/components/Common/OgTag.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import img from 'src/assets/images/310223374_1734544413588410_4745229975174853381_n.jpg';
 import imgFavicon from '/public/static/favicon_io/favicon.ico';
 interface IOG {
   title: string;
@@ -14,8 +12,8 @@ function OgTag({ title }: IOG) {
       <meta property="og:locale" content="vi_GB" />
       <meta property="og:locale:alternate" content="vi-VN" />
 
-      <meta property="og:image" content={img} />
-      <meta property="og:image:secure_url" content={img} />
+      {/* <meta property="og:image" content={img} />
+      <meta property="og:image:secure_url" content={img} /> */}
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="300" />
